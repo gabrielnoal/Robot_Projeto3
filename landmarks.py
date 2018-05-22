0d7202ff243120c78a3dd44b026a18ccf2ee91b8
Gabriel Oliva <gabriel.oliva@hotmail.com>
2018-05-22 17:12:34 -0300

    WIP

--- landmarks.py
+++ landmarks.py
@@ -4,9 +4,9 @@
 #Import required modules
 import cv2
 import dlib
-
+import numpy as np
 #Dlib positions
-#  ("mouth", (48, 68)),
+#  ("mouth", (48, 54)),
 #	("right_eyebrow", (17, 22)),
 #	("left_eyebrow", (22, 27)),
 #	("right_eye", (36, 42)),
@@ -14,21 +14,22 @@ import dlib
 #	("nose", (27, 35)),
 #	("jaw", (0, 17))
 
+
+
 #Set up some required objects
 video_capture = cv2.VideoCapture(1) #Webcam object
 #Change Frame Rate
 #video_capture.set(cv2.cv.CV_CAP_PROP_FPS, 60)
 #Change Resolution
-video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320);
-video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 130);
+video_capture.set(cv2.CV_CAP_PROP_FRAME_WIDTH, 320);
+video_capture.set(cv2.CV_CAP_PROP_FRAME_WIDTH, 130);
 
 detector = dlib.get_frontal_face_detector() #Face detector
 #Landmark identifier. Set the filename to whatever you named the downloaded file
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") 
-
 while True:
     ret, frame = video_capture.read()
-    landmark = frame - frame
+    landmark = np.zeros_like(frame)
     #frame = cv2.flip(frame,180)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
@@ -39,9 +40,10 @@ while True:
     for k,d in enumerate(detections): #For each detected face  
         shape = predictor(clahe_image, d) #Get coordinates
         for i in range(1,68): #There are 68 landmark points on each face
-            if i == 7:
-                cv2.circle(landmark, (shape.part(i).x, shape.part(i).y), 2, (255,255,255), thickness=-1) #For each point, draw a red circle with thickness2 on the original frame
-            else:    
+            if i == 48:
+                cv2.circle(landmark, (shape.part(i).x, shape.part(land_i).y), 2, (255,0,0), thickness=-1) #For each point, draw a red circle with thickness2 on the original frame
+
+            else:
                 cv2.circle(landmark, (shape.part(i).x, shape.part(i).y), 2, (0,255,0), thickness=-1) #For each point, draw a red circle with thickness2 on the original frame
             #cv2.putText(frame, str(i), (shape.part(i).x,shape.part(i).y),
             #        fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
@@ -53,5 +55,8 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'): #Exit program when the user presses 'q'
         break
+
+
+    
     
 cv2.destroyAllWindows()
