a1b686ecacfd0273ebb9cb8d1d3469d3bec85b49
Gabriel Oliva <gabriel.oliva@hotmail.com>
2018-05-23 16:49:29 -0300

    WIP

--- landmarks.py
+++ landmarks.py
@@ -9,8 +9,6 @@ import math
 
 
 
-#VECTOR1 = 48=>54  #BOCA INTEIRA
-
 '''Figure 6.4. Visualisation of ang1 angle.'''
 #VECTOR1 = 48=>63
 #VECTOR2 = 48=>67
@@ -28,15 +26,11 @@ import math
 #VECTOR1 = 57=>48    #ESQUERDO
 #VECTOR2 = 57=>54  #DIREITO
 
+'''Figure 6.8. Visualisation of ang5 angle'''
+#VECTOR1 = 51=>31
+#VECTOR2 = 51=>35
 
-def dotproduct(v1, v2):
-  return sum((a*b) for a, b in zip(v1, v2))
 
-def length(v):
-  return math.sqrt(dotproduct(v, v))
-
-def angle(v1, v2):
-  return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
 
     
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -44,7 +38,7 @@ fontScale = 0.4
 fontColor = (255,255,255)
 lineType = 2
 #Set up some required objects
-video_capture = cv2.VideoCapture(1) #Webcam object
+video_capture = cv2.VideoCapture(0) #Webcam object
 #Change Frame Rate
 #video_capture.set(cv2.cv.CV_CAP_PROP_FPS, 60)
 #Change Resolution
@@ -67,13 +61,8 @@ while True:
     for k,d in enumerate(detections): #For each detected face  
         shape = predictor(clahe_image, d) #Get coordinates
         for i in range(1,68): #There are 68 landmark points on each face
-            if i == 48 or i == 67 or i == 57 or i == 54:
-                cv2.circle(landmark, (shape.part(i).x, shape.part(i).y), 2, (255,0,0), thickness=-1) #For each point, draw a red circle with thickness2 on the original frame
-
-            else:
-                cv2.circle(landmark, (shape.part(i).x, shape.part(i).y), 2, (0,255,0), thickness=-1) #For each point, draw a red circle with thickness2 on the original frame
-                cv2.putText(landmark, str(i), (shape.part(i).x, shape.part(i).y), font,fontScale,fontColor,lineType)
-            #cv2.putText(frame, str(i), (shape.part(i).x,shape.part(i).y),
+            cv2.circle(landmark, (shape.part(i).x, shape.part(i).y), 2, (0,255,0), thickness=-1) #For each point, draw a red circle with thickness2 on the original frame
+            cv2.putText(landmark, str(i), (shape.part(i).x, shape.part(i).y), font,fontScale,fontColor,lineType)
             #        fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
             #        fontScale=0.3,
             #        color=(0, 0, 255))
--- read_landmarks.py
+++ read_landmarks.py
@@ -16,16 +16,21 @@ def load_images_from_folder(folder):
             images.append(img)
     return
 
-def dotproduct(v1, v2):
-      return sum((a*b) for a, b in zip(v1, v2))
 
-def length(v):
-  return math.sqrt(dotproduct(v, v))
+def angle_between(p0,p1,p2):
+    v0 = np.array(p0) - np.array(p1)
+    v1 = np.array(p2) - np.array(p1)
+    angle = np.math.atan2(np.linalg.det([v0,v1]),np.dot(v0,v1))
+    return angle #in radians
 
-def angle(v1, v2):
-  return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
 
 images =[]
+
+angles1 =[]
+angles2 =[]
+angles3 =[]
+angles4 =[]
+angles5 =[]
 
 
 images_happy = load_images_from_folder("emotions/happy/")
@@ -40,18 +45,86 @@ foto=0
 print("Numero de imagens = {}".format(len(images)))
 for img in images:
     foto += 1
-    #img_black = np.zeros_like(img)
+    img_black = np.zeros_like(img)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     clahe_image = clahe.apply(gray)
 
     detections = detector(clahe_image, 1) #Detect the faces in the image
-
+    
+    point31 = []
+    point33 = []
+    point35 = []
+    point48 = []
+    point51 = []
+    point54 = []
+    point57 = []
+    point63 = []
+    point67 = []
     for k,d in enumerate(detections): #For each detected face  
         shape = predictor(clahe_image, d) #Get coordinates
+
+
+        
+        
         for i in range(1,68): #There are 68 landmark points on each face
+            if i == 31:
+                point31.append(shape.part(i).x)
+                point31.append(shape.part(i).y)
+            if i == 33:
+                point33.append(shape.part(i).x)
+                point33.append(shape.part(i).y)
+            if i == 35:
+                point35.append(shape.part(i).x)
+                point35.append(shape.part(i).y)
+            if i == 48:
+                point48.append(shape.part(i).x)
+                point48.append(shape.part(i).y)
+            if i == 51:
+                point51.append(shape.part(i).x)
+                point51.append(shape.part(i).y)
+            if i == 54:
+                point54.append(shape.part(i).x)
+                point54.append(shape.part(i).y)
+            if i == 57:
+                point57.append(shape.part(i).x)
+                point57.append(shape.part(i).y)
+            if i == 63:
+                point63.append(shape.part(i).x)
+                point63.append(shape.part(i).y)
+            if i == 67:
+                point67.append(shape.part(i).x)
+                point67.append(shape.part(i).y)
+            
+            
             cv2.circle(img, (shape.part(i).x, shape.part(i).y), 2, (0,255,0), thickness=-1) #For each point, draw a red circle with thickness2 on the original frame
-    print(foto)
-    cv2.imwrite("emotions/landmarks{0}.jpg".format(foto), img) #Display the frame
     
+    angle63_48_67 = angle_between(point63,point48,point67)
+    #cv2.line(img,(point48[0],point48[1]),(point63[0],point63[1]),(255,0,0),thickness=-1)
+    #cv2.line(img,(point48[0],point48[1]),(point67[0],point67[1]),(255,0,0),thickness=-1)
+    angles1.append(angle63_48_67)
+
+    angle33_48_63 = angle_between(point33,point48,point63)
+    #cv2.line(img,(point48[0],point48[1]),(point33[0],point33[1]),(0,255,0),thickness=-1)
+    #cv2.line(img,(point48[0],point48[1]),(point63[0],point63[1]),(0,255,0),thickness=-1)
+    angles2.append(angle33_48_63)
+
+    angle31_48_54 = angle_between(point31,point48,point54)
+    #cv2.line(img,(point48[0],point48[1]),(point31[0],point31[1]),(0,0,255),thickness=-1)
+    #cv2.line(img,(point48[0],point48[1]),(point54[0],point54[1]),(0,0,255),thickness=-1)
+    angles3.append(angle31_48_54)
+
+    angle48_57_54 = angle_between(point48,point57,point54)
+    #cv2.line(img,(point57[0],point57[1]),(point48[0],point48[1]),(255,255,255),thickness=-1)
+    #cv2.line(img,(point57[0],point57[1]),(point54[0],point54[1]),(255,255,255),thickness=-1)
+    angles4.append(angle48_57_54)
+
+    angle31_51_35 = angle_between(point31,point51,point35)
+    #cv2.line(img,(point51[0],point51[1]),(point31[0],point31[1]),(255,0,0),thickness=-1)
+    #cv2.line(img,(point51[0],point51[1]),(point35[0],point35[1]),(255,0,0),thickness=-1)
+    angles5.append(angle31_51_35)
+
+    cv2.imwrite("emotions/landmarks{0}.jpg".format(foto), img) #Display the frame
+    print("Fotos prontas: {0}/{1}".format(foto,len(images)))
+
